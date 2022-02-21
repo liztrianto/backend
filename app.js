eb8b1ddb8d88866a1e7461f1992f1755aa0100fe
liztrianto <53510112+liztrianto@users.noreply.github.com>
2022-02-21 11:06:02 +0700

    backend node.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    const artikel = {
+        id:1,
+        judul: 'Cara Memperbaiki Rem Cakram Motor yang Blong',
+        // isi: 'Cara memperbaiki rem cakram motor yang blong terbilang mudah. Anda hanya perlu mengikuti langkah-langkah yang ditetapkan. Dilansir dari WikiHow, berikut ini langkah-langkah mengganti rem cakram motor yang sudah rusak. 1. Buka kaliper remSiapkan kunci ukuran 8, 10, 12 mm atau yang sesuai dengan baut kaliper rem motor Anda. Buka kaliper rem dan kelompokkan baut agar tidak tertukar dengan baut lainnya.',
+        imageUrl:'https://akcdn.detik.net.id/community/media/visual/2018/09/07/1e57dbcf-ab47-459c-b7db-cc4a4511af12_169.jpeg?w=620'
+    }
+
+    const dataartikel = []
+    data.push(artikel)
+   
+
+    res.send(dataartikel)
+    // res.send('Hello World!')
+    // res.send(artikel)
+})
+
+app.get('/test', (req, res) => {
+
+    const dataartikel = []
+    data.push(artikel)
+    
+
+    res.send(artikel)
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ article.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/test', (req, res, next) => {
+
+    const dataartikel=[
+        {
+            id:'1',
+            judul: 'Cara Memperbaiki Rem Cakram Motor yang Blong',
+            isi: 'Cara memperbaiki rem cakram motor yang blong terbilang mudah. Anda hanya perlu mengikuti langkah langkah yang ditetapkan. Dilansir dari WikiHow berikut ini langkah-langkah mengganti rem cakram motor yang sudah rusak.',
+            imageurl:'https://akcdn.detik.net.id/community/media/visual/2018/09/07/1e57dbcf-ab47-459c-b7db-cc4a4511af12_169.jpeg?w=620'
+        },
+        {
+            id:'2',
+            judul: 'Cara Merawat HP Android Agar Optimal dan Memperpanjang Masa Pakai',
+            isi: 'Umur ponsel tidak hanya bergantung pada spesifikasi yang ada pada perangkatnya, namun cara penggunaan. Penggunaan ponsel Android yang sembarangan bisa memperpendek usianya.',
+            imageurl:'https://akcdn.detik.net.id/visual/2022/01/04/samsung-galaxy-s21-fe-5g_169.png?w=650'
+        },
+        {
+            id:'3',
+            judul: 'Cara Merawat HP Android Agar Optimal dan Memperpanjang Masa Pakai',
+            isi: 'Umur ponsel tidak hanya bergantung pada spesifikasi yang ada pada perangkatnya, namun cara penggunaan. Penggunaan ponsel Android yang sembarangan bisa memperpendek usianya.',
+            imageurl:'https://akcdn.detik.net.id/visual/2022/01/04/samsung-galaxy-s21-fe-5g_169.png?w=650'
+        },
+        {
+            id:'4',
+            judul: 'Cara Merawat HP Android Agar Optimal dan Memperpanjang Masa Pakai',
+            isi: 'Umur ponsel tidak hanya bergantung pada spesifikasi yang ada pada perangkatnya, namun cara penggunaan. Penggunaan ponsel Android yang sembarangan bisa memperpendek usianya.',
+            imageurl:'https://akcdn.detik.net.id/visual/2022/01/04/samsung-galaxy-s21-fe-5g_169.png?w=650'
+        }
+        
+    ]
+
+    // const dataartikel = {data:data}
+    res.send(dataartikel)
+
+    
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
